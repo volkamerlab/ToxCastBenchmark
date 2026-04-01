dc9c46c8efa0e59d71e31798de775288b0473b87
Lisa-Marie Rolli <lisa-marie.rolli@uni-saarland.de>
2026-04-01 16:20:01 +0200

    added plots for maccs pca + corrected bug in plotting scripts

--- evaluation/generate_DR_method_comparison_across_and_per_model.py
+++ evaluation/generate_DR_method_comparison_across_and_per_model.py
@@ -63,7 +63,7 @@ def cd_plot_for_nemenyi(final_results, dr_name_map, feature_type, feature_name_m
     # generate p-value heat map
     plt.close('all')
 
-    norm = TwoSlopeNorm(vmin=pvals.values.min(),
+    norm = TwoSlopeNorm(vmin=min(pvals.values.min(), 0.04),
                         vmax=pvals.values.max(), vcenter=0.05)
 
     cmap = LinearSegmentedColormap.from_list(
